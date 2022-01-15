062a8d1cb31674ae1fceaf4ffc61589d221738a2
Jacob Cho <jacobcho@live.com>
2022-01-15 09:37:02 +0000

    function calculate results

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert(1)
+//list for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults)
+
+//calculate results
+function calculateResults(e){
+  console.log('Calculating...')
+
+  e.preventDefault() //form submit, so we prevent default
+}
